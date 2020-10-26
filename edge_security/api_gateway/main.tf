3a1fdedb41191eeeb114e6d940bd0551a3afc2fc
chimbs86 <gaurav.raje07@gmail.com>
2020-10-26 13:36:26 -0400

    added random

--- edge_security/api_gateway/main.tf
+++ edge_security/api_gateway/main.tf
@@ -15,3 +15,26 @@ resource "aws_api_gateway_method" "GetCampaignId" {
   http_method   = "GET"
   authorization = "NONE"
 }
+
+resource "aws_api_gateway_integration" "MyDemoIntegration" {
+  rest_api_id          = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+  resource_id          = aws_api_gateway_resource.GetCampaignIdResource.id
+  http_method          = aws_api_gateway_method.GetCampaignId.http_method
+  type                 = "MOCK"
+  cache_key_parameters = ["method.request.path.param"]
+  cache_namespace      = "foobar"
+  timeout_milliseconds = 29000
+
+  request_parameters = {
+    "integration.request.header.X-Authorization" = "'static'"
+  }
+
+  # Transforms the incoming XML request to JSON
+  request_templates = {
+    "application/xml" = <<EOF
+{
+   "body" : $input.json('$')
+}
+EOF
+  }
+}
