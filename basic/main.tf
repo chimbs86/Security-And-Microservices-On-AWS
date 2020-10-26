7feb425587490b566cf83d8105710655bdde9c89
chimbs86 <gaurav.raje07@gmail.com>
2020-10-26 13:28:41 -0400

    API Gateway fixed path

--- basic/main.tf
+++ basic/main.tf
@@ -1,17 +1,1 @@
-resource "aws_api_gateway_rest_api" "ECommerceMarketingAPI" {
-  name        = "MyDemoAPI"
-  description = "This is my API for demonstration purposes"
-}
 
-resource "aws_api_gateway_resource" "GetCampaignIdResource" {
-  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-  parent_id   = aws_api_gateway_rest_api.ECommerceMarketingAPI.root_resource_id
-  path_part   = "getCapmaignId"
-}
-
-resource "aws_api_gateway_method" "MyDemoMethod" {
-  rest_api_id   = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-  resource_id   = aws_api_gateway_resource.GetCampaignIdResource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
