3c14d4ef009c15462314da16d80be5cebce7652b
chimbs86 <gaurav.raje07@gmail.com>
2020-10-26 20:32:11 -0400

    nacls

--- networking/security_groups_and_nacls/nacls.tf
+++ networking/security_groups_and_nacls/nacls.tf
@@ -1,24 +1,30 @@
-
-
 resource "aws_network_acl" "block_smb_port" {
   vpc_id = aws_vpc.production_customer.id
-subnet_ids = [aws_subnet.production_customer_a.id]
+  subnet_ids = [
+    aws_subnet.production_customer_a.id]
   egress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    from_port  = 443
-    to_port    = 443
+    protocol = "tcp"
+    rule_no = 100
+    action = "allow"
+    from_port = 443
+    to_port = 443
     cidr_block = "0.0.0.0/0"
   }
-
   ingress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
+    protocol = "tcp"
+    rule_no = 100
+    action = "allow"
+    from_port = 443
+    to_port = 443
     cidr_block = "0.0.0.0/0"
-    from_port  = 80
-    to_port    = 100
+  }
+  egress {
+    protocol = "tcp"
+    rule_no = 100
+    action = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port = 445
+    to_port = 445
   }
 
   tags = {
