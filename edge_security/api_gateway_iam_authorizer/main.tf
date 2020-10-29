cdad7a5010c9bc6f59dcf32bf148428f4b715585
chimbs86 <gaurav.raje07@gmail.com>
2020-10-28 23:08:44 -0400

    us east

--- edge_security/api_gateway_iam_authorizer/main.tf
+++ edge_security/api_gateway_iam_authorizer/main.tf
@@ -1,82 +1,30 @@
-//resource "aws_api_gateway_rest_api" "ECommerceMarketingAPI" {
-//  name        = "Campaign management API Test"
-//  description = "Contains all methods related to campaign management"
-//}
-//
-//resource "aws_api_gateway_resource" "GetCampaignIdResource" {
-//  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-//  parent_id   = aws_api_gateway_rest_api.ECommerceMarketingAPI.root_resource_id
-//  path_part   = "getCapmaignId"
-//}
-//
-//resource "aws_api_gateway_method" "GetCampaignId" {
-//  rest_api_id   = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-//  resource_id   = aws_api_gateway_resource.GetCampaignIdResource.id
-//  http_method   = "GET"
-//  authorization = "NONE"
-//}
-//
-//
-//resource "aws_api_gateway_integration" "MyDemoIntegration" {
-//  rest_api_id          = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-//  resource_id          = aws_api_gateway_resource.GetCampaignIdResource.id
-//  http_method          = aws_api_gateway_method.GetCampaignId.http_method
-//  type                 = "MOCK"
-//  timeout_milliseconds = 29000
-//
-//
-//}
-
-resource "aws_api_gateway_stage" "test" {
-  stage_name    = "prod"
-  rest_api_id   = aws_api_gateway_rest_api.test.id
-  deployment_id = aws_api_gateway_deployment.test.id
+resource "aws_api_gateway_rest_api" "ECommerceMarketingAPI" {
+  name        = "Campaign management API Test"
+  description = "Contains all methoeds related to campaign management"
 }
 
-resource "aws_api_gateway_rest_api" "test" {
-  name        = "MyDemoAPI"
-  description = "This is my API for demonstration purposes"
+resource "aws_api_gateway_resource" "GetCampaignIdResource" {
+  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+  parent_id   = aws_api_gateway_rest_api.ECommerceMarketingAPI.root_resource_id
+  path_part   = "getCapmaignId"
 }
 
-resource "aws_api_gateway_deployment" "test" {
-  depends_on  = [aws_api_gateway_integration.test]
-  rest_api_id = aws_api_gateway_rest_api.test.id
-  stage_name  = "dev"
-}
-
-resource "aws_api_gateway_resource" "test" {
-  rest_api_id = aws_api_gateway_rest_api.test.id
-  parent_id   = aws_api_gateway_rest_api.test.root_resource_id
-  path_part   = "mytestresource"
-}
-
-resource "aws_api_gateway_method" "test" {
-  rest_api_id   = aws_api_gateway_rest_api.test.id
-  resource_id   = aws_api_gateway_resource.test.id
+resource "aws_api_gateway_method" "GetCampaignId" {
+  rest_api_id   = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+  resource_id   = aws_api_gateway_resource.GetCampaignIdResource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_settings" "s" {
-  rest_api_id = aws_api_gateway_rest_api.test.id
-  stage_name  = aws_api_gateway_stage.test.stage_name
-  method_path = "${aws_api_gateway_resource.test.path_part}/${aws_api_gateway_method.test.http_method}"
 
-  settings {
-    metrics_enabled = true
-
-  }
+resource "aws_api_gateway_integration" "MyDemoIntegration" {
+  rest_api_id          = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+  resource_id          = aws_api_gateway_resource.GetCampaignIdResource.id
+  http_method          = aws_api_gateway_method.GetCampaignId.http_method
+  type                 = "MOCK"
+  timeout_milliseconds = 29000
+request_templates = {
+  "application/json" = {"statusCode": 200}
 }
 
-resource "aws_api_gateway_integration" "test" {
-  rest_api_id = aws_api_gateway_rest_api.test.id
-  resource_id = aws_api_gateway_resource.test.id
-  http_method = aws_api_gateway_method.test.http_method
-  type        = "MOCK"
-
-  request_templates = {
-
-      statusCode = 200
-
-  }
 }
