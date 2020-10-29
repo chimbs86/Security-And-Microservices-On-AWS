aaee9c2388138ce9502774b7934d21c747d4268f
chimbs86 <gaurav.raje07@gmail.com>
2020-10-28 22:47:02 -0400

    us east

--- edge_security/api_gateway_iam_authorizer/main.tf
+++ edge_security/api_gateway_iam_authorizer/main.tf
@@ -27,43 +27,50 @@
 //
 //}
 
-resource "aws_api_gateway_rest_api" "MyDemoAPI" {
+resource "aws_api_gateway_stage" "test" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.test.id
+  deployment_id = aws_api_gateway_deployment.test.id
+}
+
+resource "aws_api_gateway_rest_api" "test" {
   name        = "MyDemoAPI"
   description = "This is my API for demonstration purposes"
 }
 
-resource "aws_api_gateway_resource" "MyDemoResource" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  parent_id   = aws_api_gateway_rest_api.MyDemoAPI.root_resource_id
-  path_part   = "mydemoresource"
+resource "aws_api_gateway_deployment" "test" {
+  depends_on  = [aws_api_gateway_integration.test]
+  rest_api_id = aws_api_gateway_rest_api.test.id
+  stage_name  = "dev"
 }
 
-resource "aws_api_gateway_method" "MyDemoMethod" {
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id   = aws_api_gateway_resource.MyDemoResource.id
+resource "aws_api_gateway_resource" "test" {
+  rest_api_id = aws_api_gateway_rest_api.test.id
+  parent_id   = aws_api_gateway_rest_api.test.root_resource_id
+  path_part   = "mytestresource"
+}
+
+resource "aws_api_gateway_method" "test" {
+  rest_api_id   = aws_api_gateway_rest_api.test.id
+  resource_id   = aws_api_gateway_resource.test.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "MyDemoIntegration" {
-  rest_api_id          = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id          = aws_api_gateway_resource.MyDemoResource.id
-  http_method          = aws_api_gateway_method.MyDemoMethod.http_method
-  type                 = "MOCK"
-  cache_key_parameters = ["method.request.path.param"]
-  cache_namespace      = "foobar"
-  timeout_milliseconds = 29000
+resource "aws_api_gateway_method_settings" "s" {
+  rest_api_id = aws_api_gateway_rest_api.test.id
+  stage_name  = aws_api_gateway_stage.test.stage_name
+  method_path = "${aws_api_gateway_resource.test.path_part}/${aws_api_gateway_method.test.http_method}"
 
-  request_parameters = {
-    "integration.request.header.X-Authorization" = "'static'"
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
   }
-
-  # Transforms the incoming XML request to JSON
-  request_templates = {
-    "application/xml" = <<EOF
-{
-   "body" : $input.json('$')
 }
-EOF
-  }
+
+resource "aws_api_gateway_integration" "test" {
+  rest_api_id = aws_api_gateway_rest_api.test.id
+  resource_id = aws_api_gateway_resource.test.id
+  http_method = aws_api_gateway_method.test.http_method
+  type        = "MOCK"
 }
