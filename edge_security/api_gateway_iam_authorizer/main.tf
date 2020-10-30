93133efb6997300a1a754e4302072d8951f97016
chimbs86 <gaurav.raje07@gmail.com>
2020-10-29 20:10:59 -0400

    us east

--- edge_security/api_gateway_iam_authorizer/main.tf
+++ edge_security/api_gateway_iam_authorizer/main.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_rest_api" "ECommerceMarketingAPI" {
-  name = "Campaign management API Test"
+  name = "Campaign management API Test2"
   description = "Contains all methoeds related to campaign management"
 }
 
