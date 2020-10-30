866fead90f60d398a04e742d3179bd9496a539e6
chimbs86 <gaurav.raje07@gmail.com>
2020-10-29 21:44:15 -0400

    us east

--- edge_security/api_gateway_iam_authorizer/deployment.tf
+++ edge_security/api_gateway_iam_authorizer/deployment.tf
@@ -1,10 +1,10 @@
-resource "aws_api_gateway_deployment" "GetCampaignDeployment" {
-  depends_on = [aws_api_gateway_integration.GetCamoaignIntegration]
-
-  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-  stage_name  = "test2"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+//resource "aws_api_gateway_deployment" "GetCampaignDeployment" {
+//  depends_on = [aws_api_gateway_integration.GetCamoaignIntegration]
+//
+//  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+//  stage_name  = "test2"
+//
+//  lifecycle {
+//    create_before_destroy = true
+//  }
+//}
