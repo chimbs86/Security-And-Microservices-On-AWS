8c2eeabc9c4c4d2d065e4c738674e20e85eca518
chimbs86 <gaurav.raje07@gmail.com>
2020-10-29 21:35:26 -0400

    us east

--- edge_security/api_gateway_iam_authorizer/main.tf
+++ edge_security/api_gateway_iam_authorizer/main.tf
@@ -49,3 +49,11 @@ resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
 EOF
   }
 }
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+  resource_id = aws_api_gateway_resource.GetCampaignIdResource.id
+  http_method = aws_api_gateway_method.GetCampaignId.http_method
+  status_code = "200"
+}
+
