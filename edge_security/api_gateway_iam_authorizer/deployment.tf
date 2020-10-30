4bb13348833b9975407519f4c10bf1913febc24e
chimbs86 <gaurav.raje07@gmail.com>
2020-10-29 21:47:30 -0400

    us east

--- edge_security/api_gateway_iam_authorizer/deployment.tf
+++ edge_security/api_gateway_iam_authorizer/deployment.tf
@@ -1,11 +1,11 @@
-resource "aws_api_gateway_deployment" "GetCampaignDeployment" {
-  depends_on = [aws_api_gateway_integration.GetCamoaignIntegration]
-
-  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-  stage_name  = "test2"
-
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+//resource "aws_api_gateway_deployment" "GetCampaignDeployment" {
+//  depends_on = [aws_api_gateway_integration.GetCamoaignIntegration, aws_api_gateway_integration_response.MyDemoIntegrationResponse]
+//
+//  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+//  stage_name  = "test2"
+//
+//
+//  lifecycle {
+//    create_before_destroy = true
+//  }
+//}
--- edge_security/api_gateway_iam_authorizer/main.tf
+++ edge_security/api_gateway_iam_authorizer/main.tf
@@ -1,59 +1,61 @@
-resource "aws_api_gateway_rest_api" "ECommerceMarketingAPI" {
-  name = "Campaign management API Test2"
-  description = "Contains all methoeds related to campaign management"
-}
-
-resource "aws_api_gateway_resource" "GetCampaignIdResource" {
-  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-  parent_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.root_resource_id
-  path_part = "getCampaignId"
-}
-
-resource "aws_api_gateway_method" "GetCampaignId" {
-  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-  resource_id = aws_api_gateway_resource.GetCampaignIdResource.id
-  http_method = "GET"
-  authorization = "NONE"
-}
-
-
-resource "aws_api_gateway_integration" "GetCamoaignIntegration" {
-  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-  resource_id = aws_api_gateway_resource.GetCampaignIdResource.id
-  http_method = aws_api_gateway_method.GetCampaignId.http_method
-  type = "MOCK"
-  timeout_milliseconds = 29000
-  request_templates = {
-    "application/json" = <<EOF
-{
-    "statusCode": 200
-}
-EOF
-  }
-  passthrough_behavior = "WHEN_NO_MATCH"
-}
-
-
-resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
-  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-  resource_id = aws_api_gateway_resource.GetCampaignIdResource.id
-  http_method = aws_api_gateway_method.GetCampaignId.http_method
-  status_code = 200
-
-  # Transforms the backend JSON response to XML
-  response_templates = {
-    "application/json" = <<EOF
-{
-    "statusCode": 200
-}
-EOF
-  }
-}
-
-resource "aws_api_gateway_method_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
-  resource_id = aws_api_gateway_resource.GetCampaignIdResource.id
-  http_method = aws_api_gateway_method.GetCampaignId.http_method
-  status_code = "200"
-}
-
+//resource "aws_api_gateway_rest_api" "ECommerceMarketingAPI" {
+//  name = "Campaign management API Test2"
+//  description = "Contains all methoeds related to campaign management"
+//}
+//
+//resource "aws_api_gateway_resource" "GetCampaignIdResource" {
+//  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+//  parent_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.root_resource_id
+//  path_part = "getCampaignId"
+//}
+//
+//resource "aws_api_gateway_method" "GetCampaignId" {
+//  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+//  resource_id = aws_api_gateway_resource.GetCampaignIdResource.id
+//  http_method = "GET"
+//  authorization = "NONE"
+//}
+//
+//
+//resource "aws_api_gateway_integration" "GetCamoaignIntegration" {
+//  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+//  resource_id = aws_api_gateway_resource.GetCampaignIdResource.id
+//  http_method = aws_api_gateway_method.GetCampaignId.http_method
+//  type = "MOCK"
+//  timeout_milliseconds = 29000
+//  request_templates = {
+//    "application/json" = <<EOF
+//{
+//    "statusCode": 200
+//}
+//EOF
+//  }
+//  passthrough_behavior = "WHEN_NO_MATCH"
+//}
+//
+//
+//resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+//  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+//  resource_id = aws_api_gateway_resource.GetCampaignIdResource.id
+//  http_method = aws_api_gateway_method.GetCampaignId.http_method
+//  status_code = 200
+//
+//
+//  # Transforms the backend JSON response to XML
+//  response_templates = {
+//    "application/json" = <<EOF
+//{
+//    "statusCode": 200
+//}
+//EOF
+//  }
+//}
+//
+//resource "aws_api_gateway_method_response" "response_200" {
+//  rest_api_id = aws_api_gateway_rest_api.ECommerceMarketingAPI.id
+//  resource_id = aws_api_gateway_resource.GetCampaignIdResource.id
+//  http_method = aws_api_gateway_method.GetCampaignId.http_method
+//  status_code = "200"
+//  depends_on = [aws_api_gateway_integration_response.MyDemoIntegrationResponse]
+//}
+//
