b46d6996e1aa0c65179f247e0ccee4cb4542ebe8
chimbs86 <gaurav.raje07@gmail.com>
2020-11-02 00:17:51 -0500

    us east

--- transit_security/private_zones/production.tf
+++ transit_security/private_zones/production.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "production" {
 resource "aws_subnet" "production_customer" {
   vpc_id = aws_vpc.production.id
   cidr_block = "10.0.0.0/24"
- map_public_ip_on_launch = true
+  map_public_ip_on_launch = true
   tags = {
     Name = "production"
   }
@@ -41,24 +41,25 @@ resource "aws_default_route_table" "production_route" {
 }
 
 
-
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
 
 
   filter {
-    name   = "owner-alias"
-    values = ["amazon"]
+    name = "owner-alias"
+    values = [
+      "amazon"]
   }
-  owners =[ "amazon"]
+  owners = [
+    "amazon"]
 
 
   filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
+    name = "name"
+    values = [
+      "amzn2-ami-hvm*"]
   }
 }
-
 
 
 resource "aws_iam_instance_profile" "test_profile" {
@@ -89,23 +90,25 @@ EOF
 
 
 resource "aws_security_group" "allow_all" {
-  name        = "allow_tls"
+  name = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.production.id
+  vpc_id = aws_vpc.production.id
 
   ingress {
     description = "TLS from VPC"
-    from_port   = 0
-    to_port     = 65665
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 65665
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 
   tags = {
@@ -121,7 +124,8 @@ resource "aws_instance" "test" {
   instance_type = "t2.micro"
   key_name = aws_key_pair.deployer.id
 
-  security_groups = [aws_security_group.allow_all]
+  vpc_security_group_ids = [
+    aws_security_group.allow_all.id]
   subnet_id = aws_subnet.production_customer.id
 
 }
@@ -130,7 +134,6 @@ resource "aws_key_pair" "deployer" {
   key_name = "deployer-key-pair"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZ1wy14AkG3HP+jqWFDdj5MVvruW6tN56VvYwnb9R+CCJMhkDtYanqKooPpiE4zPIWy1zKz9M1oz61MKTs6cosPMdj2C5uuEl0snVbAQqv1KMY8JS/fUN6jYiIonv8zs5s1aTJ6LhhrTpFE0iLpV4oTwQmd8UrGJ5nm2lIZD7vGCthdG3ZMLhKSUZefnY8hWb0WFS6kM5GtCVKsYVMgHD3nwISs9eMhVhl1OpNGbo6/yKj98xJI52WBzCjC2cO4QCrPg5JnVdO2Kg9EHUPw3a1GNnUpDZim9YcdvDYMVfy64dETJOZSe8b/CGiQGdUHXvWssR9K6gMCnBacmLkHirrk/roVoICMN5WVBF9VpZpCsWC+RbVz0IxNE0d9zax/1PuYjaHfM7ZnNgKIocx3nYYJ+/J3qzxIv2D1RSWP5gCcf6BeobSntgEDeyp79NbarqJt7KB8XjQsYz6dfTKbmCMp8/k2r0PXbH1x7QV9nbhX8k8Hv+Qe3eVq3g5Y0HLKM0= gaurav.raje07@gmail.com"
 }
-
 
 
 resource "aws_route53_zone" "private" {
@@ -143,8 +146,9 @@ resource "aws_route53_zone" "private" {
 
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.private.id
-  name    = "www.example.com"
-  type    = "A"
-  ttl     = "300"
-  records = ["54.162.115.122"]
+  name = "www.example.com"
+  type = "A"
+  ttl = "300"
+  records = [
+    "54.162.115.122"]
 }
