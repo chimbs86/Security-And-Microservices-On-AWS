eeba15559ebf6e48f97dbe49e8e74d41aecd2d6d
chimbs86 <gaurav.raje07@gmail.com>
2020-11-02 00:20:57 -0500

    us east

--- transit_security/private_zones/production.tf
+++ transit_security/private_zones/production.tf
@@ -97,7 +97,7 @@ resource "aws_security_group" "allow_all" {
   ingress {
     description = "TLS from VPC"
     from_port = 0
-    to_port = 65665
+    to_port = 65535
     protocol = "tcp"
     cidr_blocks = [
       "0.0.0.0/0"]
