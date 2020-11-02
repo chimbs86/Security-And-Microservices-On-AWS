dfd8f823672dc0ad2617809c78ffb197b873fbb1
chimbs86 <gaurav.raje07@gmail.com>
2020-11-02 00:11:29 -0500

    us east

--- transit_security/private_zones/production.tf
+++ transit_security/private_zones/production.tf
@@ -36,11 +36,6 @@ resource "aws_default_route_table" "production_route" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
-  route {
-    cidr_block = aws_subnet.production_customer.cidr_block
-
-  }
-
 
   default_route_table_id = aws_vpc.production.default_route_table_id
 }
@@ -93,6 +88,30 @@ EOF
 }
 
 
+resource "aws_security_group" "allow_all" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.production.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 0
+    to_port     = 65665
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
 resource "aws_instance" "test" {
 
 
@@ -102,9 +121,11 @@ resource "aws_instance" "test" {
   instance_type = "t2.micro"
   key_name = aws_key_pair.deployer.id
 
+  security_groups = [aws_security_group.allow_all]
   subnet_id = aws_subnet.production_customer.id
 
 }
+
 resource "aws_key_pair" "deployer" {
   key_name = "deployer-key-pair"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZ1wy14AkG3HP+jqWFDdj5MVvruW6tN56VvYwnb9R+CCJMhkDtYanqKooPpiE4zPIWy1zKz9M1oz61MKTs6cosPMdj2C5uuEl0snVbAQqv1KMY8JS/fUN6jYiIonv8zs5s1aTJ6LhhrTpFE0iLpV4oTwQmd8UrGJ5nm2lIZD7vGCthdG3ZMLhKSUZefnY8hWb0WFS6kM5GtCVKsYVMgHD3nwISs9eMhVhl1OpNGbo6/yKj98xJI52WBzCjC2cO4QCrPg5JnVdO2Kg9EHUPw3a1GNnUpDZim9YcdvDYMVfy64dETJOZSe8b/CGiQGdUHXvWssR9K6gMCnBacmLkHirrk/roVoICMN5WVBF9VpZpCsWC+RbVz0IxNE0d9zax/1PuYjaHfM7ZnNgKIocx3nYYJ+/J3qzxIv2D1RSWP5gCcf6BeobSntgEDeyp79NbarqJt7KB8XjQsYz6dfTKbmCMp8/k2r0PXbH1x7QV9nbhX8k8Hv+Qe3eVq3g5Y0HLKM0= gaurav.raje07@gmail.com"
