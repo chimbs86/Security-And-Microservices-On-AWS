9f9bf64002a2a87ed2d4fa97b91d87e4888c9770
chimbs86 <gaurav.raje07@gmail.com>
2020-11-14 21:10:55 -0500

    cloudfront

--- edge_security/cloudfront/cloudfront.tf
+++ edge_security/cloudfront/cloudfront.tf
@@ -17,7 +17,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     origin_id   = local.s3_origin_id
 
     s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/testoriginchimbs"
+      origin_access_identity = "origin-access-identity/cloudfront"
     }
   }
 
