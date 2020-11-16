83064354300345a413844a911040bfd54cff0547
chimbs86 <gaurav.raje07@gmail.com>
2020-11-16 00:44:09 -0500

    cloudfront

--- edge_security/cloudfront/cloudfront.tf
+++ edge_security/cloudfront/cloudfront.tf
@@ -17,7 +17,7 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
 
 resource "aws_acm_certificate" "example" {
   domain_name       = "fallacyis.com"
-  validation_method = "EMAIL"
+  validation_method = "DNS"
 
   tags = {
     Environment = "test"
@@ -125,4 +125,31 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     acm_certificate_arn = aws_acm_certificate.example.arn
     ssl_support_method = "sni-only"
   }
+}
+
+data "aws_route53_zone" "example" {
+  name         = "fallacyis.com"
+  private_zone = false
+}
+
+resource "aws_route53_record" "example" {
+  for_each = {
+  for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    name   = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type   = dvo.resource_record_type
+  }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.example.zone_id
+}
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.example.arn
+  validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
 }
