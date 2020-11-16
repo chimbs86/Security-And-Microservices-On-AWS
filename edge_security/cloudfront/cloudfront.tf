4beb8db593f2d4bd63c2910782dfb437449007ef
chimbs86 <gaurav.raje07@gmail.com>
2020-11-16 00:55:08 -0500

    cloudfront

--- edge_security/cloudfront/cloudfront.tf
+++ edge_security/cloudfront/cloudfront.tf
@@ -19,6 +19,7 @@ resource "aws_acm_certificate" "example" {
   domain_name       = "fallacyis.com"
   validation_method = "DNS"
 
+
   tags = {
     Environment = "test"
   }
@@ -147,7 +148,7 @@ resource "aws_route53_record" "cert_validation" {
   allow_overwrite = true
   name            = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_name
   records         = [ tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_value ]
-  type            = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_type
+  type            = "CNAME"
   zone_id  = data.aws_route53_zone.public.id
   ttl      = 60
 }
