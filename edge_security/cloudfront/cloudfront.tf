d5ed4a812ad3c2d4d496d2afb706118d96825826
chimbs86 <gaurav.raje07@gmail.com>
2020-11-16 00:52:58 -0500

    cloudfront

--- edge_security/cloudfront/cloudfront.tf
+++ edge_security/cloudfront/cloudfront.tf
@@ -127,29 +127,39 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 }
 
-data "aws_route53_zone" "example" {
+
+
+data "aws_route53_zone" "public" {
   name         = "fallacyis.com"
   private_zone = false
 }
 
-resource "aws_route53_record" "example" {
-  for_each = {
-  for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
-    record = dvo.resource_record_value
-    type   = dvo.resource_record_type
-  }
-  }
 
+
+# This is a DNS record for the ACM certificate validation to prove we own the domain
+#
+# This example, we make an assumption that the certificate is for a single domain name so can just use the first value of the
+# domain_validation_options.  It allows the terraform to apply without having to be targeted.
+# This is somewhat less complex than the example at https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate_validation
+# - that above example, won't apply without targeting
+
+resource "aws_route53_record" "cert_validation" {
   allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.example.zone_id
+  name            = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_name
+  records         = [ tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_value ]
+  type            = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_type
+  zone_id  = data.aws_route53_zone.public.id
+  ttl      = 60
 }
 
-resource "aws_acm_certificate_validation" "example" {
+# This tells terraform to cause the route53 validation to happen
+resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.example.arn
-  validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
+  validation_record_fqdns = [ aws_route53_record.cert_validation.fqdn ]
+}
+
+
+
+output "testing" {
+  value = "Test this demo code by going to "
 }
