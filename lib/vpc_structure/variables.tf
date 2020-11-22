42ce86724696978dc7dae2d691601757601dc0fd
chimbs86 <gaurav.raje07@gmail.com>
2020-11-22 01:08:07 -0500

    internet gateway

--- lib/vpc_structure/variables.tf
+++ lib/vpc_structure/variables.tf
@@ -19,3 +19,22 @@ variable "finance_subnet_cidr" {
 variable "default_az" {
   default = "us-east-1a"
 }
+variable "finance_vpc_name" {
+  default = "finance"
+}
+variable "customer_vpc_name" {
+  default = "customer"
+}
+variable "marketing_vpc_name" {
+  default = "marketing"
+}
+
+variable "finance_subnet_name" {
+  default = "finance_subnet"
+}
+variable "customer_subnet_name" {
+  default = "customer_subnet"
+}
+variable "marketing_subnet_name" {
+  default = "marketing_subnet"
+}
