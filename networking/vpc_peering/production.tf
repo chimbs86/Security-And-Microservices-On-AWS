9f6c76fee8483e383c2849c942ec7758ec71ee71
chimbs86 <gaurav.raje07@gmail.com>
2020-11-22 03:44:37 -0500

    internet gateway

--- networking/vpc_peering/production.tf
+++ networking/vpc_peering/production.tf
@@ -53,3 +53,41 @@ resource "aws_main_route_table_association" "main_table_prod" {
   route_table_id = aws_route_table.finance_route.id
   vpc_id = module.vpc_structure.finance_vpc_id
 }
+
+
+resource "aws_route_table" "customer_route" {
+  vpc_id = module.vpc_structure.customer_vpc_id
+  tags = {
+    Name = "Customer Route Table"
+  }
+  route {
+    cidr_block = local.finance.finance_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.customer_finance_peering_connection.id
+  }
+
+
+
+}
+resource "aws_main_route_table_association" "main_table_customer" {
+  route_table_id = aws_route_table.customer_route.id
+  vpc_id = module.vpc_structure.customer_vpc_id
+}
+
+
+resource "aws_route_table" "marketing_route" {
+  vpc_id = module.vpc_structure.marketing_vpc_id
+  tags = {
+    Name = "marketing Route Table"
+  }
+  route {
+    cidr_block = local.finance.finance_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.marketing_finance_peering_connection.id
+  }
+
+
+
+}
+resource "aws_main_route_table_association" "main_table_prod" {
+  route_table_id = aws_route_table.marketing_route.id
+  vpc_id = module.vpc_structure.marketing_vpc_id
+}
