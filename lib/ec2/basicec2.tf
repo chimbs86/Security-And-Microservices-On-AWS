c66cdb4a27561ad73ce2d38e80a545b50f664c50
chimbs86 <gaurav.raje07@gmail.com>
2020-11-22 12:24:54 -0500

    internet gateway

--- lib/ec2/basicec2.tf
+++ lib/ec2/basicec2.tf
@@ -72,6 +72,17 @@ resource "aws_security_group_rule" "allow_outgoing" {
   cidr_blocks = [
     "0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "allow_outgoing_ping" {
+  from_port = 0
+  protocol = "icmp"
+  security_group_id = aws_security_group.allow_all.id
+  to_port = 0
+  type = "egress"
+  cidr_blocks = [
+    "0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "allow_pings" {
   from_port = 0
   protocol = "icmp"
