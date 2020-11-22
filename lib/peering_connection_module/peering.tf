a5a946d7c5c6b2ed1b3882e4f58c2babeb33379b
chimbs86 <gaurav.raje07@gmail.com>
2020-11-22 11:59:06 -0500

    internet gateway

--- lib/peering_connection_module/peering.tf
+++ lib/peering_connection_module/peering.tf
@@ -1,12 +1,12 @@
 resource "aws_route" "main_route" {
-  cidr_block = var.main_cidr_block
   vpc_peering_connection_id = var.peering_connection_id
   route_table_id = var.route_table_main_id
+  destination_cidr_block = var.main_cidr_block
 }
 
 
 resource "aws_route" "customer_route" {
-  cidr_block = var.acceptor_cidr_block
+  destination_cidr_block = var.acceptor_cidr_block
   vpc_peering_connection_id = var.peering_connection_id
   route_table_id = var.route_table_acceptor_id
 }
