596a5bd5eadd247627ba30ecf64d242c36394a00
chimbs86 <gaurav.raje07@gmail.com>
2020-11-22 00:25:39 -0500

    internet gateway

--- transit_security/route53/vpc/production.tf
+++ transit_security/route53/vpc/production.tf
@@ -24,3 +24,22 @@ module "ec2" {
   subnet_id = aws_subnet.subnet.id
   vpc_id = aws_vpc.vpc.id
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "production_route" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+
+
+}
+resource "aws_main_route_table_association" "main_table_prod"{
+  route_table_id = aws_route_table.production_route.id
+  vpc_id = aws_vpc.vpc.id
+}
