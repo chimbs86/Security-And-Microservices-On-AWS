c65f2187478fb4a915a72b3161b78ccd816e9f61
chimbs86 <gaurav.raje07@gmail.com>
2020-11-22 19:26:32 -0500

    load balancer

--- lib/ec2/basicec2.tf
+++ lib/ec2/basicec2.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "test" {
     aws_security_group.allow_all.id]
   subnet_id = var.subnet_id
 
-  user_data = file("./userdata.sh")
+  user_data = "sudo yum -y install httpd && sudo service httpd start"
 
   tags = {
     Name ="${var.vpc_name} -  ${random_id.id.hex}"
