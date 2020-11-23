2dbcaa76c5e5a161e1d0bf413eb5e60498bc3bdc
chimbs86 <gaurav.raje07@gmail.com>
2020-11-22 21:26:00 -0500

    load balancer

--- transit_security/private_zones/production.tf
+++ transit_security/private_zones/production.tf
@@ -1,148 +1,28 @@
-resource "aws_vpc" "production" {
-  cidr_block = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  tags = {
-    Name = "production"
-  }
+module "vpc_structure" {
+  source = "../../lib/vpc_structure"
+  customer_subnet_cidr = local.customer.customer_subnet_cidr
+  customer_vpc_cidr = local.customer.customer_vpc_cidr
+  finance_subnet_cidr = local.finance.finance_subnet_cidr
+  finance_vpc_cidr = local.finance.finance_vpc_cidr
+  marketing_subnet_cidr = local.marketing.marketing_subnet_cidr
+  marketing_vpc_cidr = local.marketing.marketing_vpc_cidr
+  ec2_inside_each = 0
 }
 
-
-resource "aws_subnet" "production_customer" {
-  vpc_id = aws_vpc.production.id
-  cidr_block = "10.0.0.0/24"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "production"
-  }
+module "ec2_customer" {
+  source = "../../lib/ec2"
+  route_table_id = module.vpc_structure.customer_route_table_id
+  subnet_id = module.vpc_structure.customer_subnet_id
+  vpc_id = module.vpc_structure.customer_vpc_id
+  vpc_name = "customer"
 }
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.production.id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-
-resource "aws_default_route_table" "production_route" {
-  tags = {
-    Name = "Production Route Table"
-  }
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  default_route_table_id = aws_vpc.production.default_route_table_id
-}
-
-
-data "aws_ami" "amazon-linux-2" {
-  most_recent = true
-
-
-  filter {
-    name = "owner-alias"
-    values = [
-      "amazon"]
-  }
-  owners = [
-    "amazon"]
-
-
-  filter {
-    name = "name"
-    values = [
-      "amzn2-ami-hvm*"]
-  }
-}
-
-
-resource "aws_iam_instance_profile" "test_profile" {
-  name = "test_profile"
-  role = aws_iam_role.role.name
-}
-
-resource "aws_iam_role" "role" {
-  name = "test_role"
-  path = "/"
-
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-               "Service": "ec2.amazonaws.com"
-            },
-            "Effect": "Allow",
-            "Sid": ""
-        }
-    ]
-}
-EOF
-}
-
-
-resource "aws_security_group" "allow_all" {
-  name = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id = aws_vpc.production.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port = 0
-    to_port = 65535
-    protocol = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-//
-//resource "aws_instance" "test" {
-//
-//
-//  ami = data.aws_ami.amazon-linux-2.id
-//  associate_public_ip_address = true
-//  iam_instance_profile = aws_iam_instance_profile.test_profile.id
-//  instance_type = "t2.micro"
-//  key_name = aws_key_pair.deployer.id
-//
-//  vpc_security_group_ids = [
-//    aws_security_group.allow_all.id]
-//  subnet_id = aws_subnet.production_customer.id
-//
-//}
-
-resource "aws_key_pair" "deployer" {
-  key_name = "deployer-key-pair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZ1wy14AkG3HP+jqWFDdj5MVvruW6tN56VvYwnb9R+CCJMhkDtYanqKooPpiE4zPIWy1zKz9M1oz61MKTs6cosPMdj2C5uuEl0snVbAQqv1KMY8JS/fUN6jYiIonv8zs5s1aTJ6LhhrTpFE0iLpV4oTwQmd8UrGJ5nm2lIZD7vGCthdG3ZMLhKSUZefnY8hWb0WFS6kM5GtCVKsYVMgHD3nwISs9eMhVhl1OpNGbo6/yKj98xJI52WBzCjC2cO4QCrPg5JnVdO2Kg9EHUPw3a1GNnUpDZim9YcdvDYMVfy64dETJOZSe8b/CGiQGdUHXvWssR9K6gMCnBacmLkHirrk/roVoICMN5WVBF9VpZpCsWC+RbVz0IxNE0d9zax/1PuYjaHfM7ZnNgKIocx3nYYJ+/J3qzxIv2D1RSWP5gCcf6BeobSntgEDeyp79NbarqJt7KB8XjQsYz6dfTKbmCMp8/k2r0PXbH1x7QV9nbhX8k8Hv+Qe3eVq3g5Y0HLKM0= gaurav.raje07@gmail.com"
-}
-
 
 resource "aws_route53_zone" "private" {
   name = "google.com"
 
 
   vpc {
-    vpc_id = aws_vpc.production.id
+    vpc_id = module.vpc_structure.customer_vpc_id
   }
 }
 
@@ -151,6 +31,5 @@ resource "aws_route53_record" "www" {
   name = "google.com"
   type = "A"
   ttl = "5"
-  records = [
-    "34.226.135.72"]
+  records = [module.ec2_customer.ip]
 }
