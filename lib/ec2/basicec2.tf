4ed3274b409ca9c4e34536be0e682bd5f6b69ef1
chimbs86 <gaurav.raje07@gmail.com>
2020-11-22 19:14:07 -0500

    load balancer

--- lib/ec2/basicec2.tf
+++ lib/ec2/basicec2.tf
@@ -74,7 +74,7 @@ resource "aws_security_group_rule" "allow_outgoing" {
   from_port = 0
   protocol = "tcp"
   security_group_id = aws_security_group.allow_all.id
-  to_port = 0
+  to_port = 65535
   type = "egress"
   cidr_blocks = [
     "0.0.0.0/0"]
@@ -85,18 +85,18 @@ resource "aws_security_group_rule" "allow_outgoing_ping" {
   protocol = "icmp"
 
   security_group_id = aws_security_group.allow_all.id
-  to_port = 0
+  to_port = 65535
   type = "egress"
   cidr_blocks = [
     "0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "allow_pings" {
-  from_port = -1
+  from_port = 0
 
   protocol = "icmp"
   security_group_id = aws_security_group.allow_all.id
-  to_port = -1
+  to_port = 65535
   type = "ingress"
   cidr_blocks = [
     "0.0.0.0/0"]
