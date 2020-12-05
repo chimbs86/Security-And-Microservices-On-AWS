cee05d79a1ca7fe6f7f87323820bffdb1c40f137
chimbs86 <gaurav.raje07@gmail.com>
2020-12-05 12:09:35 -0500

    KMS Keys

--- encryption/kms_basics/kms.tf
+++ encryption/kms_basics/kms.tf
@@ -1,6 +1,6 @@
 resource "aws_kms_key" "symmetric" {
   description             = "KMS key 1"
-  deletion_window_in_days = 1
+  deletion_window_in_days = 7
   customer_master_key_spec = "SYMMETRIC_DEFAULT" // this is the default value
   enable_key_rotation =  false
 
@@ -9,7 +9,7 @@ resource "aws_kms_key" "symmetric" {
 
 resource "aws_kms_key" "asymmetric" {
   description             = "KMS key 1"
-  deletion_window_in_days = 1
+  deletion_window_in_days = 7
   customer_master_key_spec = "ECC_SECG_P256K1" // this is the default value
   enable_key_rotation =  false
 
