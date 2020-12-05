03d69f7438c1b91ea92bcb2a47269678918252ff
chimbs86 <gaurav.raje07@gmail.com>
2020-12-05 12:07:10 -0500

    KMS Keys

--- encryption/kms_basics/main.tf
+++ encryption/kms_basics/main.tf
@@ -1,3 +1,15 @@
-module "main" {
-  source = "../../basic"
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
 }
+
+provider "aws" {
+  version = "~> 3.0"
+  region = var.aws_region
+}
+
+
+provider "random" {
+  version = "2.2"
+}
+
