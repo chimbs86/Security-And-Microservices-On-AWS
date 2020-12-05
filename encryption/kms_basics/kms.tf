4194cf25fca09fa95a7df6c1fa252ecfd759c718
chimbs86 <gaurav.raje07@gmail.com>
2020-12-05 12:17:06 -0500

    KMS Keys

--- encryption/kms_basics/kms.tf
+++ encryption/kms_basics/kms.tf
@@ -11,10 +11,14 @@ resource "aws_kms_key" "asymmetric" {
   tags = {
     Name = "Sample-Asymmetric-Key"
   }
-  alias = "Asymmetric"
   description             = "KMS key 1"
   deletion_window_in_days = 7
   customer_master_key_spec = "RSA_4096"
   enable_key_rotation =  false
 
+}
+
+resource "aws_kms_alias" "asymmetric_key_alias" {
+  name          = "alias/asymmetric-key-alias"
+  target_key_id = aws_kms_key.asymmetric.id
 }
