55d42e16fa9818bd83445d27569a54904efa731a
chimbs86 <gaurav.raje07@gmail.com>
2021-05-07 10:44:06 -0400

    test

--- plaid_exchange/api_gateway.tf
+++ plaid_exchange/api_gateway.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "p_e_vpc" {
 }
 
 resource "aws_subnet" "p_e_subnet" {
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
   vpc_id     = aws_vpc.p_e_vpc.id
   cidr_block = "10.0.0.0/24"
 
@@ -46,7 +46,7 @@ resource "aws_default_security_group" "default" {
 resource "aws_apigatewayv2_vpc_link" "p_e_link" {
   name               = "p_e_link"
   security_group_ids = [aws_default_security_group.default.id]
-  subnet_ids         = aws_subnet.p_e_subnet.id
+  subnet_ids         = [aws_subnet.p_e_subnet.id]
 
 
   tags = {
